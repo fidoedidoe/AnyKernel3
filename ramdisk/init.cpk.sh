8f607cdd64f76496c1835763b61019667b567cb7
fidoedidoe <gmf@pebbleglow.co.uk>
2020-09-05 17:24:51 +0100

    init.rc: add persist.cpk.cpu_voltage to log output + tidy up

--- ramdisk/init.cpk.sh
+++ ramdisk/init.cpk.sh
@@ -40,19 +40,27 @@ function write() {
 
    # If there is no persistent values we need to set them
    if [ ! -f /data/property/persist.cpk.zzm_profile ] || [ $UNSTABLE -eq 1 ]; then
-       setprop persist.cpk.cpu_gov "zzmoove"
-   fi
-   
-   if [ ! -f /data/property/persist.cpk.zzm_profile ] || [ $UNSTABLE -eq 1 ]; then
        setprop persist.cpk.zzm_profile "8"
    fi
 
-   if [ ! -f /data/property/persist.cpk.cpu_min ] || [ $UNSTABLE -eq 1 ]; then
+   if [ ! -f /data/property/persist.cpk.zzm_profile ] || [ $UNSTABLE -eq 1 ]; then
+       setprop persist.cpk.cpu_gov "zzmoove"
+   fi
+   
+if [ ! -f /data/property/persist.cpk.cpu_min ] || [ $UNSTABLE -eq 1 ]; then
        setprop persist.cpk.cpu_min "200000"
    fi
 
    if [ ! -f /data/property/persist.cpk.cpu_max ] || [ $UNSTABLE -eq 1 ]; then
        setprop persist.cpk.cpu_max "1600000"
+   fi
+   
+   if [ ! -f /data/property/persist.cpk.cpu_voltage ] || [ $UNSTABLE -eq 1 ]; then
+       setprop persist.cpk.cpu_voltage "0"
+   fi
+   
+   if [ ! -f /data/property/persist.cpk.cpu_idle ] || [ $UNSTABLE -eq 1 ]; then
+       setprop persist.cpk.cpu_idle "3"
    fi
    
    if [ ! -f /data/property/persist.cpk.gpu_profile ] || [ $UNSTABLE -eq 1 ]; then
@@ -67,10 +75,6 @@ function write() {
        setprop persist.cpk.external_ra_kb "0"
    fi
 
-   if [ ! -f /data/property/persist.cpk.cpu_idle ] || [ $UNSTABLE -eq 1 ]; then
-       setprop persist.cpk.cpu_idle "3"
-   fi
-
    if [ ! -f /data/property/persist.cpk.module_debug ] || [ $UNSTABLE -eq 1 ]; then
        setprop persist.cpk.module_debug "0"
    fi
@@ -81,10 +85,6 @@ function write() {
 
    if [ ! -f /data/property/persist.cpk.io_scheduler ] || [ $UNSTABLE -eq 1 ]; then
        setprop persist.cpk.io_scheduler "row"
-   fi
-
-   if [ ! -f /data/property/persist.cpk.cpu_voltage ] || [ $UNSTABLE -eq 1 ]; then
-       setprop persist.cpk.cpu_voltage "0"
    fi
 
    #Read property values and set them again 
@@ -98,14 +98,15 @@ function write() {
    $L "### init.cpk.sh: Getting properties"
    $L "### ------------------------------"
 
-   cpu_gov=$(getprop persist.cpk.cpu_gov)
    zzm_profile=$(getprop persist.cpk.zzm_profile)
+   cpu_gov=$(getprop persist.cpk.cpu_gov)
    cpu_min=$(getprop persist.cpk.cpu_min)
    cpu_max=$(getprop persist.cpk.cpu_max)
+   cpu_voltage=$(getprop persist.cpk.cpu_voltage)
+   cpu_idle=$(getprop persist.cpk.cpu_idle)
    gpu_profile=$(getprop persist.cpk.gpu_profile)
    internal_ra_kb=$(getprop persist.cpk.internal_ra_kb)
    external_ra_kb=$(getprop persist.cpk.external_ra_kb)
-   cpu_idle=$(getprop persist.cpk.cpu_idle)
    module_debug=$(getprop persist.cpk.module_debug)
    system_tweaks=$(getprop persist.cpk.system_tweaks)
    io_scheduler=$(getprop persist.cpk.io_scheduler)
@@ -116,27 +117,29 @@ function write() {
 
    $L "### -------------------------------"
    $L "### init.cpk.sh: Setting properties"
-   $L "###   setprop persist.cpk.cpu_gov $cpu_gov"
    $L "###   setprop persist.cpk.zzm_profile $zzm_profile"
+   $L "###   setprop persist.cpk.cpu_gov $cpu_gov"
    $L "###   setprop persist.cpk.cpu_min $cpu_min"
    $L "###   setprop persist.cpk.cpu_max $cpu_max"
+   $L "###   setprop persist.cpk.cpu_voltage $cpu_voltage"
+   $L "###   setprop persist.cpk.cpu_idle $cpu_idle"
    $L "###   setprop persist.cpk.gpu_profile $gpu_profile"
    $L "###   setprop persist.cpk.internal_ra_kb $internal_ra_kb"
    $L "###   setprop persist.cpk.external_ra_kb $external_ra_kb"
-   $L "###   setprop persist.cpk.cpu_idle $cpu_idle"
    $L "###   setprop persist.cpk.module_debug $module_debug"
    $L "###   setprop persist.cpk.system_tweaks $system_tweaks"
    $L "###   setprop persist.cpk.io_scheduler $io_scheduler"
    $L "### -------------------------------"
 
-   setprop persist.cpk.cpu_gov $cpu_gov
    setprop persist.cpk.zzm_profile $zzm_profile
+   setprop persist.cpk.cpu_gov $cpu_gov
    setprop persist.cpk.cpu_min $cpu_min
    setprop persist.cpk.cpu_max $cpu_max
+   setprop persist.cpk.cpu_max $cpu_voltage
+   setprop persist.cpk.cpu_idle $cpu_idle
    setprop persist.cpk.gpu_profile $gpu_profile
    setprop persist.cpk.internal_ra_kb $internal_ra_kb
    setprop persist.cpk.external_ra_kb $external_ra_kb
-   setprop persist.cpk.cpu_idle $cpu_idle
    setprop persist.cpk.module_debug $module_debug
    setprop persist.cpk.system_tweaks $system_tweaks
    setprop persist.cpk.io_scheduler $io_scheduler
